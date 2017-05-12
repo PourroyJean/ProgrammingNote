582b23d83177746a807187baaec272724bdb4605
Ubuntu <jean@Nano-ubuntu-VM.ielbyy3bjwuuredtcfjnooi3gd.ax.internal.cloudapp.net>
2017-05-12 14:15:53 +0000

    sphinx server is here

---
+++ sphinx-server/_build/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:50,filenames:["index.txt"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,pag:0,recherch:0},titles:["Welcome to JeanDocumentation&#8217;s documentation!"],titleterms:{and:0,document:0,indic:0,jeandocument:0,tabl:0,welcom:0}})
